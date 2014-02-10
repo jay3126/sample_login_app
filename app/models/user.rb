cd21138dfd14a310f9162510bbb3390b38001b7e
Janmejay Rai <janmejay.rai@rsystems.com>
2014-02-10 21:27:43 +0530

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,57 +4,57 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
 	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-		user = User.where(:provider => auth.provider, :uid => auth.uid).first
+		user = User.where(provider: auth.provider, uid: auth.uid).first
 		if user
 			return user
 		else
-			registered_user = User.where(:email => auth.info.email).first
+			registered_user = User.where(email: auth.info.email).first
 			if registered_user
 				return registered_user
 			else
-				user = User.create(name:auth.extra.raw_info.name,
-														provider:auth.provider,
-														uid:auth.uid,
-														email:auth.info.email,
-														password:Devise.friendly_token[0,20],
+				user = User.create(name: auth.extra.raw_info.name,
+														provider: auth.provider,
+														uid: auth.uid,
+														email: auth.info.email,
+														password: Devise.friendly_token[0,20],
 													)
 			end
 		end
 	end
 
 	def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
-		user = User.where(:provider => auth.provider, :uid => auth.uid).first
+		user = User.where(provider: auth.provider, uid: auth.uid).first
 		if user
 			return user
 		else
-			registered_user = User.where(:email => auth.uid + "@twitter.com").first
+			registered_user = User.where(email: auth.uid + "@twitter.com").first
 			if registered_user
 				return registered_user
 			else
-				user = User.create(name:auth.extra.raw_info.name,
-														provider:auth.provider,
-														uid:auth.uid,
-														email:auth.uid+"@twitter.com",
-														password:Devise.friendly_token[0,20],
+				user = User.create(name: auth.extra.raw_info.name,
+														provider: auth.provider,
+														uid: auth.uid,
+														email: auth.uid+"@twitter.com",
+														password: Devise.friendly_token[0,20],
 													)
 			end
 		end
 	end
 
 	def self.connect_to_linkedin(auth, signed_in_resource=nil)
-		user = User.where(:provider => auth.provider, :uid => auth.uid).first
+		user = User.where(provider: auth.provider, uid: auth.uid).first
 		if user
 			return user
 		else
-			registered_user = User.where(:email => auth.info.email).first
+			registered_user = User.where(email: auth.info.email).first
 			if registered_user
 				return registered_user
 			else
-				user = User.create(name:auth.info.first_name,
-														provider:auth.provider,
-														uid:auth.uid,
-														email:auth.info.email,
-														password:Devise.friendly_token[0,20],
+				user = User.create(name: auth.info.first_name,
+														provider: auth.provider,
+														uid: auth.uid,
+														email: auth.info.email,
+														password: Devise.friendly_token[0,20],
 													)
 			end
 		end
